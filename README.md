a2494230dad5cd57fc2ba34470e1cda7810b0436
Arthur REITER <arthur.reiter@univ-tlse3.fr>
2020-04-12 09:19:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kaamelott-api
+ A very simple API in NodeJS to get quotes from the french TV series Kaamelott  ## 
---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+ * API developped by Arthur REITER
+ * Website : https://reiter.tf
+ * Documentation : https://github.com/arthur-reiter/kaamelott-api
+ */
+
+// Server settings
+var hostname = 'localhost';
+var port = 8080;
+
+var express = require('express');
+
+var app = express();
+
+// All json data was extracted from https://github.com/sin0light/api-kaamelott/
+const quotes = require('./data/quotes.json');
+const casting = require('./data/casting.json');
+const persos = require('./data/persos.json');
+const nb_quotes = 776;
+
+var myRouter = express.Router();
+
+// Returns the home page
+myRouter.route('/')
+	.get(function (req, res) {
+		res.sendfile('index.html');
+	});
+
+// Returns a random quote
+myRouter.route('/api/random')
+	.get(function (req, res) {
+		var id = Math.floor(Math.random() * Math.floor(nb_quotes));
+		res.json(quotes[id]);
+	});
+
+// Returns the quotation corresponding to the id
+myRouter.route('/api/quote/:quote_id')
+	.get(function (req, res) {
+		res.json(quotes[req.params.quote_id]);
+	});
+
+// Returns all quotes
+myRouter.route('/api/all')
+	.get(function (req, res) {
+		res.json(quotes);
+	});
+
+app.use(myRouter);
+
+app.listen(port, hostname, function () {
+	console.log("Server work on http://" + hostname + ":" + port);
+});
