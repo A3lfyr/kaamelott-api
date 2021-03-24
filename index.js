9be471051590d4445986c10bcd394e63fc457b30
Arthur REITER <arthur.reiter231@gmail.com>
2021-03-24 09:28:25 +0100

    rename router

--- index.js
+++ index.js
@@ -28,41 +28,41 @@ var app = express();
 app.use(cors());
 app.use(express.static('public'));
 
+var router = express.Router();
+
+
 // All json data was extracted from https://github.com/sin0light/api-kaamelott/
 const quotes = require('./data/quotes.json');
 const casting = require('./data/casting.json');
 const persos = require('./data/persos.json');
-const nb_quotes = 776;
-
-var myRouter = express.Router();
-
+const nb_quotes = quotes.length;
 
 // Returns the home page
-myRouter.route('/')
+router.route('/')
 	.get(function (req, res) {
 		res.sendFile('index.html');
 	});
 
 // Returns a random quote
-myRouter.route('/api/random')
+router.route('/api/random')
 	.get(function (req, res) {
 		var id = Math.floor(Math.random() * Math.floor(nb_quotes));
 		res.json(quotes[id]);
 	});
 
 // Returns the quotation corresponding to the id
-myRouter.route('/api/quote/:quote_id')
+router.route('/api/quote/:quote_id')
 	.get(function (req, res) {
 		res.json(quotes[req.params.quote_id]);
 	});
 
 // Returns all quotes
-myRouter.route('/api/all')
+router.route('/api/all')
 	.get(function (req, res) {
 		res.json(quotes);
 	});
 
-app.use(myRouter);
+app.use(router);
 
 app.listen(port, hostname, function () {
 	console.log("Server work on http://" + hostname + ":" + port);
